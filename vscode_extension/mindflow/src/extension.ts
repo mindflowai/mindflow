dd64bdecce0e26f490ce3e90de78091f0fab12f3
McCrearyD <mccreary@dyllan.ai>
2022-12-09 18:38:06 -0800

    refac

--- vscode_extension/mindflow/src/extension.ts
+++ vscode_extension/mindflow/src/extension.ts
@@ -2,6 +2,40 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
+function openQuery() {
+	// If there is no active terminal, create a new one
+	if (!vscode.window.activeTerminal) {
+		vscode.window.createTerminal();
+	}
+
+
+	// Open a search bar that is overlaying the current editor
+	vscode.window.showInputBox({
+		placeHolder: "Search for a note",
+		prompt: "Search for a note",
+		ignoreFocusOut: true
+	}).then((value) => {
+		// treat the value as a command and execute it in the terminal
+
+		if (!value) {
+			return;
+		}
+
+		let terminal = vscode.window.activeTerminal;
+
+		// If there is no active terminal, create a new one
+		if (!terminal) {
+			terminal = vscode.window.createTerminal();
+		}
+
+		terminal.sendText(value);
+		// console.log(value);
+
+		
+	});
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,42 +47,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('mindflow.query', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from mindflow!');
-			
-		// If there is no active terminal, create a new one
-		if (!vscode.window.activeTerminal) {
-			vscode.window.createTerminal();
-		}
-
-
-		// Open a search bar that is overlaying the current editor
-		vscode.window.showInputBox({
-			placeHolder: "Search for a note",
-			prompt: "Search for a note",
-			ignoreFocusOut: true
-		}).then((value) => {
-			// treat the value as a command and execute it in the terminal
-
-			if (!value) {
-				return;
-			}
-
-			let terminal = vscode.window.activeTerminal;
-
-			// If there is no active terminal, create a new one
-			if (!terminal) {
-				terminal = vscode.window.createTerminal();
-			}
-
-			terminal.sendText(value);
-			// console.log(value);
-
-			
-		});
-	});
+	let disposable = vscode.commands.registerCommand('mindflow.query', openQuery);
 
 	context.subscriptions.push(disposable);
 }
