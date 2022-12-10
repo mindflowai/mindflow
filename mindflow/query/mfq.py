1e1442c654e5434af8ecd3d390e2aa9e6e3331f7
McCrearyD <mccreary@dyllan.ai>
2022-12-10 14:58:40 -0800

    built language parser

--- mindflow/query/mfq.py
+++ mindflow/query/mfq.py
@@ -1,8 +1,11 @@
 from typing import Set
 
+import re
+
 
 MFQ_FILE = "_mf_query.mfq"
 
+FILE_REF_REGEXES = [r"(@[\w|\/|\.|\_]+)", r"(@\[.*\])"]
 
 class MindFlowQueryHandler:
     
@@ -30,12 +33,14 @@ class MindFlowQueryHandler:
     def get_prompt(self):
 
         # first, we need to scrub all comments from the file (lines that start with `#`)
-        # do this using a regular expression
-        import re
         self.query = re.sub(r"#.*", "", self.query)
 
-        print(self.query)
         # then, we need to parse the `@` references in the text
+        refs = set()
+        for regex in FILE_REF_REGEXES:
+            refs.update(re.findall(regex, self.query))
+        refs = set(map(lambda x: x[1:], refs))
+        print(refs)
 
         # then we need to create the prompt to feed to the model.
         # for now, let's replace the `@` references with the filename, and then compose the json as normal
