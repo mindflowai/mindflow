c1c4d6499272103083aeae128025e4f0f53be7ce
McCrearyD <mccreary@dyllan.ai>
2022-12-11 12:12:30 -0800

    make package pip install-able!

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-"""
-Setup script for mindflow
-"""
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="mindflow",
-    version="0.1",
+    version="0.1.4",
     py_modules=["mindflow"],
-    entry_points={"console_scripts": ["mf = main:main"]},
+    entry_points={"console_scripts": ["mf = mindflow.main:main"]},
+    packages=find_packages(),
     install_requires=["requests", "revChatGPT", "bs4", "chardet", "pyperclip", "gitpython"],
 )
