5f2afe6da177863f6ebc4f5ff4c22e36caae8fd4
McCrearyD <mccreary@dyllan.ai>
2022-12-11 12:17:58 -0800

    0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mindflow",
-    version="0.1.4",
+    version="0.1.5",
     py_modules=["mindflow"],
     entry_points={"console_scripts": ["mf = mindflow.main:main"]},
     packages=find_packages(),
