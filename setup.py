e0587993078bedb44cd42fe027eaadb5f3d99255
McCrearyD <mccreary@dyllan.ai>
2022-12-11 11:21:27 -0800

    change version to show it's an MVP to 0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="mindflow",
-    version="1.0.0",
+    version="0.1",
     py_modules=["mindflow"],
     entry_points={"console_scripts": ["mf = main:main"]},
     install_requires=["requests", "revChatGPT", "bs4", "chardet", "pyperclip", "gitpython"],
