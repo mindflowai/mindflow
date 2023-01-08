88664d07c7f3c8504284cb5a16a3c88af9767f70
Chris Steege <steegecas@gmail.com>
2023-01-08 17:44:02 -0600

    Update comment on resolve.rs

--- mindflow/src/resolve/resolve.rs
+++ mindflow/src/resolve/resolve.rs
@@ -3,6 +3,7 @@ use crate::resolve::file_path_resolver::PathResolver;
 use super::{file_path_resolver::ResolvedFilePath};
 
 // If more resolvers are added, the program must handle the returned resolved types using dynamic dispatch.
+// Or better, I could use enums to represent the different types of resolvers which would be more efficient.
 pub(crate) async fn resolve(references: &Vec<String>) -> Vec<ResolvedFilePath> {
     let mut resolved_references: Vec<ResolvedFilePath> = Vec::new();
     for reference in references {
