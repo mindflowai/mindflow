5462c35ece20dff068ccb336e98e31d5f976c59c
Chris Steege <steegecas@gmail.com>
2023-01-16 18:27:09 -0600

    Do a little refactoring and organization

--- mindflow/resolve_handling/resolvers/path_resolver.py
+++ mindflow/resolve_handling/resolvers/path_resolver.py
@@ -2,15 +2,14 @@
 File/Directory Resolver
 """
 import hashlib
-import logging
 import os
-import codecs
 
 from typing import List, Union
 
 from mindflow.resolve_handling.resolvers.base_resolver import BaseResolver, Resolved
 from mindflow.utils.reference import Reference
-from mindflow.utils.git import is_within_git_repo, get_git_files
+from mindflow.utils.files.utf8 import is_valid_utf8
+from mindflow.utils.files.extract import extract_files
 
 
 class ResolvedPath(Resolved):
@@ -59,53 +58,20 @@ class ResolvedPath(Resolved):
         except UnicodeDecodeError:
             return None
 
-
 class PathResolver(BaseResolver):
     """
     Resolver for file or directory paths to text.
     """
-
-    def extract_files(self, path: os.PathLike) -> List[os.PathLike]:
-        """
-        Extract all files from a directory.
-        """
-        file_paths = []
-        if os.path.isfile(path):
-            return [os.fspath(path)]
-        if is_within_git_repo(path):
-            return get_git_files(path)
-        try:
-            with os.scandir(path) as entries:
-                for entry in entries:
-                    if entry.is_file():
-                        file_paths.append(os.fspath(entry.path))
-                    elif entry.is_dir():
-                        file_paths.extend(self.extract_files(entry.path))
-            return file_paths
-        except IsADirectoryError as error:
-            logging.debug("Could not read directory/file: %s", path)
-            logging.debug(error)
-            return file_paths
-
-    def is_valid_utf8(self, file_path: os.PathLike) -> bool:
-        """
-        Check if a file is valid utf8.
-        """
-        try:
-            for _ in codecs.open(file_path, encoding="utf-8", errors="strict"):
-                pass
-            return True
-        except UnicodeDecodeError:
-            return False
-
-    def should_resolve(self, reference: Union[str, os.PathLike]) -> bool:
+    @staticmethod
+    def should_resolve(reference: Union[str, os.PathLike]) -> bool:
         """
         Check if a path is a file or directory.
         """
         return os.path.isfile(reference) or os.path.isdir(reference)
 
-    def resolve(self, reference: Union[str, os.PathLike]) -> List[ResolvedPath]:
+    @staticmethod
+    def resolve(reference: Union[str, os.PathLike]) -> List[ResolvedPath]:
         """
         Extract text from files.
         """
-        return [ResolvedPath(file) for file in self.extract_files(reference) if self.is_valid_utf8(file)]
+        return [ResolvedPath(file) for file in extract_files(reference) if is_valid_utf8(file)]
---
+++ mindflow/utils/files/extract.py
@@ -0,0 +1,28 @@
+import logging
+import os
+
+from typing import List
+
+from mindflow.utils.files.git import is_within_git_repo, get_git_files
+
+def extract_files(path: os.PathLike) -> List[os.PathLike]:
+    """
+    Extract all files from a directory.
+    """
+    file_paths = []
+    if os.path.isfile(path):
+        return [os.fspath(path)]
+    if is_within_git_repo(path):
+        return get_git_files(path)
+    try:
+        with os.scandir(path) as entries:
+            for entry in entries:
+                if entry.is_file():
+                    file_paths.append(os.fspath(entry.path))
+                elif entry.is_dir():
+                    file_paths.extend(extract_files(entry.path))
+        return file_paths
+    except IsADirectoryError as error:
+        logging.debug("Could not read directory/file: %s", path)
+        logging.debug(error)
+        return file_paths
