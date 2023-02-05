136545ca7c9c9269094d34f3a4bb0b049ea2e141
McCrearyD <mccreary@dyllan.ai>
2023-02-04 21:17:56 -0800

    update requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+
+with open("requirements.txt", "r") as f:
+    install_requires = f.read().strip().split("\n")
+
 setup(
     name="mindflow",
     version="0.2.8",
     py_modules=["mindflow"],
     entry_points={"console_scripts": ["mf = mindflow.main:main"]},
     packages=find_packages(),
+    install_requires=install_requires,
 )
