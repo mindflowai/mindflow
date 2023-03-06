c0cbd3c45f74ca5e9e567fb639133ffac0af7cae
Chris Steege <steegecas@gmail.com>
2023-03-06 00:15:02 -0600

    Add functions to delete and inspect documents and update their implementation.

--- mindflow/core/delete.py
+++ mindflow/core/delete.py
@@ -11,7 +11,9 @@ def run_delete(document_paths: List[str]):
     """
     This function is used to delete your MindFlow index.
     """
-    Document.delete_bulk([
-        document_reference.path for document_reference in resolve_all(document_paths)
-    ])
+    document_to_delete = [document_reference.path for document_reference in resolve_all(document_paths)]
+    
+    Document.delete_bulk(document_to_delete)
+    
     DATABASE_CONTROLLER.databases.json.save_file()
+    return "Documents deleted"
