a8470c7449b4b87971aeae4f9424f1c18875893e
Chris Steege <steegecas@gmail.com>
2023-03-05 22:03:56 -0600

    Add has_staged_files function and modify run_commit function to use it.

--- mindflow/core/commit.py
+++ mindflow/core/commit.py
@@ -11,9 +11,11 @@ def run_commit():
     """
     settings = Settings()
 
-    print(subprocess.check_call(['git', 'diff', '--cached', '--quiet']))
-    exit()
+    if not has_staged_files():
+        print("No staged files")
+        return
 
+    # Execute the git diff command and retrieve the output as a string
     diff_output = run_diff(("--cached",))
     response: str = settings.mindflow_models.query.model(build_context_prompt(COMMIT_PROMPT_PREFIX, diff_output))
 
@@ -21,3 +23,10 @@ def run_commit():
 
     # Execute the git diff command and retrieve the output as a string
     subprocess.check_output(command).decode("utf-8")
+
+def has_staged_files():
+    try:
+        subprocess.check_call(['git', 'diff', '--cached', '--quiet'])
+        return False  # no staged files
+    except subprocess.CalledProcessError:
+        return True  # there are staged files
