a9dc206d931f3129d3ee554d32ad2e0525082526
nollied <mccreary@dyllan.ai>
2023-03-07 00:12:13 +0000

    write simple cli version test

---
+++ mindflow/unit_tests/test_cli.py
@@ -0,0 +1,11 @@
+import mindflow
+
+from click.testing import CliRunner
+from mindflow.cli.new_click_cli.cli_main import mindflow_cli
+
+
+def test_version():
+    runner = CliRunner()
+    result = runner.invoke(mindflow_cli, ["version"])
+    assert result.exit_code == 0
+    assert result.output == f"{mindflow.__version__}\n"
