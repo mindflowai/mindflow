5375a42d829f030aef204e93f2d7d20cbcc79ca3
nollied <mccreary@dyllan.ai>
2023-03-07 21:25:55 +0000

    Added new lines to the `@passthrough_command` decorator for better readability.
    Co-authored-by: MindFlow <mf@mindflo.ai>

--- mindflow/cli/new_click_cli/commands/diff.py
+++ mindflow/cli/new_click_cli/commands/diff.py
@@ -6,6 +6,9 @@ from typing import Tuple
 from mindflow.core.diff import run_diff
 from mindflow.cli.new_click_cli.util import passthrough_command
 
-@passthrough_command(help="Wrapper around git diff that summarizes the output. Treat this command exactly like git diff, it supports all arguments that git diff provides.")
+
+@passthrough_command(
+    help="Wrapper around git diff that summarizes the output. Treat this command exactly like git diff, it supports all arguments that git diff provides."
+)
 def diff(args: Tuple[str]):
     print(run_diff(args))
