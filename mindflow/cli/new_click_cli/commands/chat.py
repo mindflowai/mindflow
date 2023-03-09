71e9680c5d0935ede76868dd378058f616ddff51
dyllan2 <dyllan@allegrolabs.io>
2023-03-09 12:22:48 -0800

    make `mf chat` the go to method

--- mindflow/cli/new_click_cli/commands/chat.py
+++ mindflow/cli/new_click_cli/commands/chat.py
@@ -2,14 +2,42 @@
 `ask` command
 """
 import click
+from typing import Tuple
+import os
 
 from mindflow.core.chat import run_chat
+from mindflow.core.index import run_index
+from mindflow.core.query import run_query
 
 
-@click.command(help="")
-@click.argument("prompt", type=str)
-def chat(prompt: str):
-    """
-    This function is used to generate a prompt and then use it as a prompt for GPT bot.
-    """
-    print(run_chat(prompt))
+def _parse_chat_prompt_args(prompt_args: Tuple[str]):
+    # takes a list of strings and returns a string along with any filenames/directories that were passed.
+    # note: if the user passes a string that contains a bunch of text, with a filename/directory in the middle,
+    # those filenames/directories will be ignored and treated as plain text.
+
+    prompt = " ".join(prompt_args)  # include files/directories in prompt
+    paths = []
+
+    for arg in prompt_args:
+        # check if valid path string
+        if os.path.exists(arg):
+            paths.append(arg)
+
+    return prompt, paths
+
+
+@click.command(help="Interact with ChatGPT, you can reference files and directories by passing them as arguments. Example: `mf chat \"Please summarize this file\" path/to/file.txt`")
+@click.option("-i", "--ignore-paths", type=bool, default=False, is_flag=True)
+@click.argument("prompt_args", nargs=-1, type=str, required=True)
+def chat(prompt_args: Tuple[str], ignore_paths: bool):
+    prompt, paths = _parse_chat_prompt_args(prompt_args)
+    if ignore_paths:
+        click.echo("Note: the following paths were ignored: " + str(paths))
+        paths.clear()
+
+    if paths:
+        run_index(paths, refresh=False, verbose=False)
+        click.echo("")
+        print(run_query(paths, prompt))
+    else:
+        print(run_chat(prompt))
